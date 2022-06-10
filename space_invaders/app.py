69f9942e2f11eec5334d43b5f92d632a3f9783e6
RMITProgrammingClub <105967138+RMITProgrammingClub@users.noreply.github.com>
2022-06-10 13:15:01 +1000

    Added Scaler

--- space_invaders/app.py
+++ space_invaders/app.py
@@ -1,25 +1,52 @@
 import pygame, time
 
 
-FPS = 1400
+
+# Constants
+
+## FPS
+FPS = 144
+
+## Window dimensions
 WIDTH, HEIGHT = 480, 640
+SCALE = 1.3
+SCALE_WIDTH, SCALE_HEIGHT = WIDTH * SCALE, HEIGHT * SCALE
+
+# Main Application class
 class App():
+
+  # Initialize varibles for Application Class
   def __init__(self):
+    # Clock for FPS limiting
     self.clock = pygame.time.Clock()
-    self.prev_time = time.time()
-    self.running = True
+
+    # Time for delta time
     self.dt = 0
+    self.prev_time = time.time()
+    
+    # Pygame varibles
+    self.running = True
     self._display_surf = None
-    self.size = self.weight, self.height = WIDTH, HEIGHT
+    self.size = self.weight, self.height = SCALE_WIDTH, SCALE_HEIGHT
+
 
   def on_init(self):
+
+    #Pygame Surface initialisation
     pygame.init()
     self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
     self._running = True
 
+    #Game Logic
+    self.level = Level()
+    self.player = Player()
+
+  # Handle events
   def on_event(self, event):
     if event.type == pygame.QUIT:
       self._running = False
+
+
 
   def on_loop(self):
     self.clock.tick(FPS)
@@ -34,21 +61,23 @@ class App():
     self.player.on_loop(self.dt)
     self.level.on_loop(self.dt)
 
-
+  # blit render layers to screen surface
   def on_render(self):
     self._display_surf.blit(*self.level.get_surface())
     self._display_surf.blit(*self.player.get_surface())
+    #update display
     pygame.display.update()
 
+
   def on_cleanup(self):
+    del self.player
+    del self.background
     pygame.quit()
 
   def on_execute(self):
     if self.on_init() == False:
       self._running = False
 
-    self.level = Level()
-    self.player = Player()
     while ( self._running ):
       for event in pygame.event.get():
         self.on_event(event)
@@ -59,7 +88,7 @@ class App():
 class Level():
 
   def __init__(self):
-    self.background = pygame.image.load("sprites/Space_BG (2 frames) (64 x 64).png")
+    self.background = pygame.image.load("sprites/Space_BG (2 frames) (64 x 64).png").convert()
     self.background_a = self.background.subsurface((0,0, 64, 64))
     self.background_b = self.background.subsurface((64,0, 64, 64))
     self.timer = 0
@@ -75,14 +104,14 @@ class Level():
         else:
           self.surface.blit(pygame.transform.rotate(self.background_b, 90 * y), (x * 64, y*64))
 
-
+    self.surface = pygame.transform.scale(self.surface, (SCALE_WIDTH, SCALE_HEIGHT))
     self.surface.blit(self.background, (0,0))
     return self.surface, (0,0)
 
   def on_loop(self, dt):
-    if time.time() % 2:
+    if time.time():
       self.timer = self.timer + 1
-      if self.timer == 100:
+      if self.timer == 200:
         self.background_a, self.background_b = self.background_b, self.background_a
         self.timer = 0
 
@@ -90,22 +119,22 @@ class Level():
 class Player():
   
   def __init__(self):
-    self.x, self.y = (WIDTH/2) - 32,(4/5) * HEIGHT
+    self.x, self.y = (SCALE_WIDTH/2) - 32,(4/5) * SCALE_HEIGHT
     
     #Load the sprite sheet
-    self.sprite = pygame.image.load("sprites/Player_ship (16 x 16).png")
+    self.sprite = pygame.image.load("sprites/Player_ship (16 x 16).png").convert()
     #Get the individual sprites
     self.player_sprite_n = self.sprite.subsurface((16, 0,16,16))
     self.player_sprite_r = self.sprite.subsurface((32,0,16,16))
     self.player_sprite_l = self.sprite.subsurface((0,0,16,16))
     #scale the sprites
-    self.player_sprite_n = pygame.transform.scale(self.player_sprite_n, (64, 64))
-    self.player_sprite_r = pygame.transform.scale(self.player_sprite_r, (64, 64))
-    self.player_sprite_l = pygame.transform.scale(self.player_sprite_l, (64, 64))
+    self.player_sprite_n = pygame.transform.scale(self.player_sprite_n, (64 * SCALE, 64 * SCALE))
+    self.player_sprite_r = pygame.transform.scale(self.player_sprite_r, (64 * SCALE, 64 * SCALE))
+    self.player_sprite_l = pygame.transform.scale(self.player_sprite_l, (64 * SCALE, 64 * SCALE))
     #set the current sprite
     self.player_sprite_current = self.player_sprite_n
     self.velocity = 0
-    self.speed = 200
+    self.speed = 200 * SCALE
 
   def get_surface(self):
     return self.player_sprite_current, (self.x, self.y)
