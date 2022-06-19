175e99086a947d4662bc8f39f10e0d8c817340a0
Jelly-Dev <15014078+Jelly-Fish-Dev@users.noreply.github.com>
2022-06-19 13:58:26 +1000

    Added Enemy

--- space_invaders/app.py
+++ space_invaders/app.py
@@ -1,25 +1,59 @@
+from html import entities
 import pygame, time
 
 
-FPS = 1400
+
+# Constants
+
+## FPS
+FPS = 144
+
+## Window dimensions
 WIDTH, HEIGHT = 480, 640
+SCALE = 2
+SCALE_WIDTH, SCALE_HEIGHT = WIDTH * SCALE, HEIGHT * SCALE
+
+
+entities = []
+
+# Main Application class
 class App():
+
+  # Initialize varibles for Application Class
   def __init__(self):
+    # Clock for FPS limiting
     self.clock = pygame.time.Clock()
-    self.prev_time = time.time()
-    self.running = True
+
+    # Time for delta time
     self.dt = 0
+    self.prev_time = time.time()
+    
+    # Pygame varibles
+    self.running = True
     self._display_surf = None
-    self.size = self.weight, self.height = WIDTH, HEIGHT
+    self.size = self.weight, self.height = SCALE_WIDTH, SCALE_HEIGHT
+
 
   def on_init(self):
+
+    #Pygame Surface initialisation
     pygame.init()
-    self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+    self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE)
     self._running = True
 
+    #Game Logic
+    self.level = Level()
+    self.player = Player()
+    self.alan = Enemy(WIDTH/2, HEIGHT/2)
+
+  # Handle events
   def on_event(self, event):
     if event.type == pygame.QUIT:
       self._running = False
+    
+    self.player.on_input(self.dt, event)
+
+
 
   def on_loop(self):
     self.clock.tick(FPS)
@@ -29,26 +63,39 @@ class App():
     self.prev_time = self.now
 
     ## GAME LOGIC GOES HERE
-    self.player.on_input(self.dt, pygame.key.get_pressed())
+    for b in entities:
+      b.on_loop(self.dt)
+  
 
     self.player.on_loop(self.dt)
     self.level.on_loop(self.dt)
 
-
+  # blit render layers to screen surface
   def on_render(self):
-    self._display_surf.blit(*self.level.get_surface())
-    self._display_surf.blit(*self.player.get_surface())
+    temp_buffer = pygame.Surface((WIDTH, HEIGHT))
+    temp_buffer.blit(*self.level.get_surface())
+    temp_buffer.blit(*self.player.get_surface())
+    
+    for b in entities:
+      temp_buffer.blit(*b.get_surface())
+
+    temp_buffer.blit(*self.alan.get_surface())
+
+    temp_buffer = pygame.transform.scale(temp_buffer, (SCALE_WIDTH, SCALE_HEIGHT))
+    self._display_surf.blit(temp_buffer, (0,0))
+    #update display
     pygame.display.update()
 
+
   def on_cleanup(self):
+    del self.player
+    del self.background
     pygame.quit()
 
   def on_execute(self):
     if self.on_init() == False:
       self._running = False
 
-    self.level = Level()
-    self.player = Player()
     while ( self._running ):
       for event in pygame.event.get():
         self.on_event(event)
@@ -59,7 +106,7 @@ class App():
 class Level():
 
   def __init__(self):
-    self.background = pygame.image.load("sprites/Space_BG (2 frames) (64 x 64).png")
+    self.background = pygame.image.load("sprites/Space_BG (2 frames) (64 x 64).png").convert_alpha()
     self.background_a = self.background.subsurface((0,0, 64, 64))
     self.background_b = self.background.subsurface((64,0, 64, 64))
     self.timer = 0
@@ -68,32 +115,49 @@ class Level():
 
   def get_surface(self):
     self.surface = pygame.Surface((WIDTH,HEIGHT))
-    for y in range(0, int(HEIGHT/64)):
+    for y in range(0, int(HEIGHT/64) + 64):
       for x in range(0, int(WIDTH/64) + 64):
         if x * y % 2 == 0:
           self.surface.blit(pygame.transform.rotate(self.background_a, 90 * x), (x * 64, y*64))
         else:
-          self.surface.blit(pygame.transform.rotate(self.background_b, 90 * y), (x * 64, y*64))
-
+          self.surface.blit(pygame.transform.rotate(self.background_b, 90 * x), (x * 64, y*64))
 
     self.surface.blit(self.background, (0,0))
     return self.surface, (0,0)
 
   def on_loop(self, dt):
-    if time.time() % 2:
+    if time.time():
       self.timer = self.timer + 1
-      if self.timer == 100:
+      if self.timer == 200:
         self.background_a, self.background_b = self.background_b, self.background_a
         self.timer = 0
 
 
-class Player():
+class Entity:
+
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.sprite = None
+
+  def get_surface(self):
+    return self.sprite, (self.x, self.y)
+
+
+class Player(Entity):
   
   def __init__(self):
+
     self.x, self.y = (WIDTH/2) - 32,(4/5) * HEIGHT
-    
+    self.load_sprites()
+    #set the current sprite
+    self.sprite = self.player_sprite_n
+    self.velocity = 0
+    self.speed = 250
+
+  def load_sprites(self):
     #Load the sprite sheet
-    self.sprite = pygame.image.load("sprites/Player_ship (16 x 16).png")
+    self.sprite = pygame.image.load("sprites/Player_ship (16 x 16).png").convert_alpha()
     #Get the individual sprites
     self.player_sprite_n = self.sprite.subsurface((16, 0,16,16))
     self.player_sprite_r = self.sprite.subsurface((32,0,16,16))
@@ -102,30 +166,63 @@ class Player():
     self.player_sprite_n = pygame.transform.scale(self.player_sprite_n, (64, 64))
     self.player_sprite_r = pygame.transform.scale(self.player_sprite_r, (64, 64))
     self.player_sprite_l = pygame.transform.scale(self.player_sprite_l, (64, 64))
-    #set the current sprite
-    self.player_sprite_current = self.player_sprite_n
-    self.velocity = 0
-    self.speed = 200
 
-  def get_surface(self):
-    return self.player_sprite_current, (self.x, self.y)
+  def on_input(self, dt, event):
 
-  def on_input(self, dt, input):
-    if input[pygame.K_RIGHT]:
-      self.velocity = 1
-      self.player_sprite_current = self.player_sprite_r
-    elif input[pygame.K_LEFT]:
-      self.velocity = -1
-      self.player_sprite_current = self.player_sprite_l
-    else:
-      self.player_sprite_current = self.player_sprite_n
-      self.velocity = 0
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_RIGHT:
+        self.velocity = 1
+        self.sprite = self.player_sprite_r
+      elif event.key == pygame.K_LEFT:
+        self.velocity = -1
+        self.sprite = self.player_sprite_l
+    if event.type == pygame.KEYUP:
+        self.sprite = self.player_sprite_n
+        self.velocity = 0
     
 
   def on_loop(self, dt):
     dx = self.speed * self.velocity * dt
     if (self.velocity == -1 and self.x > 0) or (self.velocity == 1 and self.x < WIDTH-64):
       self.x += dx
+
+class Projectile(Entity):
+  def __init__(self,x,y, direction, speed):
+    super().__init__(x, y)
+    self.load_sprites()
+    self.dir = direction
+    self.speed = speed
+
+  def load_sprites(self):
+    self.sprite = pygame.image.load("sprites/Player_beam (16 x 16).png").convert_alpha()
+    self.sprite = pygame.transform.scale(self.sprite, (64,64))
+
+  def on_loop(self, dt):
+    dy = self.speed * self.dir * dt
+    self.y += dy
+    if (self.y <= 0):
+      entities.remove(self)
+
+
+  
+class Enemy(Entity):
+  def __init__(self,x,y):
+    super().__init__(x, y)
+    self.load_sprites()
+
+  def load_sprites(self):
+    self.sprite_sheet = pygame.image.load("sprites/Alan (16 x 16).png").convert_alpha()
+
+    self.sprites = []
+    self.sprites.append(self.sprite_sheet.subsurface((48,0,16,16)))
+    self.sprites.append(self.sprite_sheet.subsurface((64,0,16,16)))
+    self.sprites.append(self.sprite_sheet.subsurface((80,0,16,16)))
+
+    for i in range(len(self.sprites)):
+      self.sprites[i] = pygame.transform.scale(self.sprites[i], (64,64))
+
+    self.sprite = self.sprites[0]
+      
 
 
 if __name__ == "__main__":
